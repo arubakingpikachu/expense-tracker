bf27a2d09ab78719235f5869753218eb9cf7de85
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-05-06 18:01:01 +0800

    feat: add express-sessio

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express=require('express')
 const exphbs = require('express-handlebars')
 const methodOverride=require('method-override')
 const routes=require('./routes')
+const session = require('express-session')
 
 require('./config/mongoose')
 if (process.env.NODE_ENV !== 'production') {
@@ -16,8 +17,13 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 //middleware區
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }))
-
+app.use(methodOverride('_method'))
 
 app.use(routes)
 
