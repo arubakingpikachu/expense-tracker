8115c91f6ee67d552debbd2ced98054c6c71e620
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-05-08 04:32:35 +0800

    feat:add totalAmount

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const Record=require('../../models/record')
+const record = require('../../models/record')
 const router = express.Router()
+let totalAmount=0
 
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
   const userId = req.user._id
-  
-  
-  Record.find({userId})
-    .lean()
-    .then(record => res.render('index', { record }))
-    .catch(error => console.error(error))
-})// 純瀏覽
+  let totalAmount=0
+ try{
+  const record=await Record.find({userId}).lean()
+ console.log(record)
+ for(let item of record){
+    totalAmount+=item.amount
+ }
+ res.render('index',{record,totalAmount})
+ } catch{console.log(error)} 
+
+})// 純瀏覽及總金額
 
 module.exports=router
