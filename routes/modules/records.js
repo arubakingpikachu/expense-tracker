c587087ee27374f885f1d57fd2c6761305115140
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-05-07 20:58:01 +0800

    feat:add new post

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -5,15 +5,18 @@ const router=express.Router()
 
 router.get('/new',async (req, res) => {
   const category=await Category.find({}).lean()
-  res.render('new',{categories:category})
+  res.render('new',{category})
 }) //new的頁面
 
-router.post('/',(req,res)=>{
-  const userId=req.user._id
-  const {name,amount,date,category} = req.body       // 從 req.body 拿出表單裡的 name 資料
-  return Record.create({ name,amount,date,userId })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error))
+router.post('/',async (req,res)=>{
+  
+  const {name,amount,date,category}=req.body
+
+  const cates=await Category.find({}).lean()
+  const refCate=cates.find(cate=>{return cate.categoryName===category})
+  
+  await Record.create({name,amount,date,categoryId:refCate._id,userId:req.user._id})
+  res.redirect('/')
 })//new的機制
 
 
