a6969eca7de95608d0f15192c8a52646c65c7d62
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-05-11 22:33:55 +0800

    feat:add get edit page

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -11,38 +11,48 @@ router.get('/new',async (req, res) => {
 router.post('/',async (req,res)=>{
   
   const {name,amount,date,category}=req.body
-  const userId=req.user._id
-
-  const cates=await Category.find({}).lean()
-  const refCate=cates.find(cate=>{return cate.categoryName===category})
-
-  console.log(date)
- 
   
-  await Record.create({name,amount,date,categoryId:refCate._id,userId})
+  const userId=req.user._id
+ 
+  const refCate=await Category.findOne({categoryName:category})//找出要新增的類別
+ 
+  await Record.create({name,amount,date,categoryId:refCate._id,userId})//創造新資料，categoryId用refCate的_is
   res.redirect('/')
 })//new POST
 
 router.get('/:id/edit',async (req,res)=>{
-  const userId=req.user._id
-  const _id=req.params.id
+  const userId=req.user._id//登入使用者的id
+  const record_id=req.params.id//條目id
 
-  try{
-    const cates=await Category.find({}).lean()
-    const record=await Record.findOne({_id,userId}).lean()
-    
-    console.log(record)
-    console.log(record.date.toLocaleDateString( { year: 'numeric', month: 'long', day: 'numeric'}))
-   
-    record.date=new Date(record.date)
-
-    console.log(record.date)
-    
-
-    res.render('edit',{record})
-  }catch{console.log(error)}
+  const record=await Record.findOne({_id:record_id,userId}).lean()//取出該筆支出完整的紀錄
   
-})
+  const cate_id=record.categoryId//該筆支出的類別id
+  const cate=await Category.findOne({_id:cate_id}).lean()
+  const cateName=cate.categoryName
+
+  const category=await Category.find({}).lean()//整組類別選項
+
+  record.date=record.date.toLocaleDateString('zh-TW',{  year:'numeric',month:'numeric',
+  day: 'numeric',})
+  
+console.log(record.date)
+
+  res.render('edit',{record,cateName,category})
+ 
+  
+})//get EDIT
+
+
+router.put('/:id',async (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+
+  
+  RestData.findOne({_id,userId})
+    .then(restaurant => { return restaurant.update(req.body) })
+    .then(() => res.redirect(`/restaurants/${_id}`))
+    .catch(error => console.log(error))
+})// 儲存編輯條目
 
 
 router.delete('/:id',async (req,res)=>{
