cb9df015de8d706963d33a619fb17c6d85dba16d
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-05-12 22:51:12 +0800

    feat:add filter

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const Record=require('../../models/record')
-const record = require('../../models/record')
+const Category = require('../../models/category')
 const router = express.Router()
 let totalAmount=0
 
@@ -17,5 +17,26 @@ router.get('/',async (req, res) => {
  } catch{console.log(error)} 
 
 })// 純瀏覽及總金額
+
+router.get('/:category',async (req,res)=>{
+   const reqCate=req.params.category//req傳送過來的分類
+   const userId=req.user._id
+   let totalAmount=0
+
+   const filterCate=await Category.findOne({categoryName:reqCate}).lean()//在model找出對應需求的物件
+   const filterCateId=filterCate._id//找出物件的_id
+
+   const record=await Record.find({categoryId:filterCateId}).lean()//篩選出符合id的record
+
+   for(let item of record){
+      totalAmount+=item.amount
+   }//總金額
+
+   res.render('index',{record,totalAmount})
+
+
+
+
+})
 
 module.exports=router
